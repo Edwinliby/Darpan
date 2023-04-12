9c88e1fcff2fabfc0bf32f2035af4f89decd119c
Edwinliby <edwinliby30@gmail.com>
2023-04-12 21:17:29 +0530

    ...

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -4,7 +4,7 @@ import Countdown from "react-countdown";
 
 function Clock() {
     return (
-        <div className='w-screen flex'>
+        <div className='w-screen flex justify-center'>
             <div className="border border-gray/50 xl:mt-[8rem] pb-5 xl:pb-10 xl w-[100] xl:w-[35rem] flex flex-col justify-center items-center  text-white text-[3rem] md:text-[4rem] tracking-[1rem] font-clash">
                 <Countdown date={new Date("2023-04-26T00:00:00")} className="text-[2rem] xl:text-[4rem]" />
 
@@ -23,7 +23,7 @@ function Clock() {
                 className='hidden xl:block object-contain px-4 mt-[8rem] border border-gray/50'
             />
 
-            <div className='hidden xl:block pl-4 border grow-[1] min-w-min-[3rem] border-gray/50 p-3 mt-[8rem] text-white'>
+            <div className='hidden xl:block pl-4 border  min-w-min-[3rem] border-gray/50 p-3 mt-[8rem] text-white'>
                 <div className='flex flex-col gap-2 justify-center items-center h-[100%]'>
                     <span className='font-chakra text-[1.8rem]'>2023 <span className='font-clash'>April</span></span>
                     <span className='flex gap-2 text-[2rem] font-chakra font-bold'>26
@@ -34,7 +34,7 @@ function Clock() {
                 </div>
             </div>
 
-            <div className='hidden xl:block p-3 mt-[8rem] border border-gray/50'>
+            {/* <div className='hidden xl:block p-3 mt-[8rem] border border-gray/50'>
                 <div className='flex flex-col gap-2 justify-center items-center h-[100%]'>
                     <div className='flex gap-2 '>
                         <div className='bg-white/50 box h-2 w-2'></div>
@@ -83,7 +83,7 @@ function Clock() {
 
             <div className='hidden xl:block p-3 mt-[8rem] grow-[2] border border-gray/50 text-white italic font-clash text-[4rem] font-bold'>
                 <h1 className='fli flex items-center w-full h-full justify-center'>LE<span id="offset">T</span>'S<span id="offset"> &nbsp;&nbsp;R</span>OC<span id="offset">K</span></h1>
-            </div>
+            </div> */}
         </div>
     )
 }
