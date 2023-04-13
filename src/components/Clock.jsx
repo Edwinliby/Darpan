2232c6501fcfae06b71adb67b23b9d012d48f9d4
gladsondezy <gladson@dezyit.com>
2023-04-13 22:04:26 +0530

    optimize

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,40 +1,49 @@
-import React from 'react'
-import Image from 'next/image'
+import Image from "next/image";
 import Countdown from "react-countdown";
 
 function Clock() {
-    return (
-        <div className='w-screen flex justify-center'>
-            <div className="md:border border-gray/50 xl:mt-[8rem] pb-5 xl:pb-10 xl w-[100] xl:w-[35rem] flex flex-col justify-center items-center z-[10] text-white text-[3rem] md:text-[4rem] tracking-[1rem] font-clash">
-                <Countdown date={new Date("2023-04-26T00:00:00")} className="text-[2rem] xl:text-[4rem]" />
+  return (
+    <div className="w-screen flex justify-center">
+      <div className="md:border border-gray/50 xl:mt-[8rem] pb-5 xl:pb-10 xl w-[100] xl:w-[35rem] flex flex-col justify-center items-center z-[10] text-white text-[3rem] md:text-[4rem] tracking-[1rem] font-clash">
+        <Countdown
+          date={new Date("2023-04-26T00:00:00")}
+          className="text-[2rem] xl:text-[4rem]"
+        />
 
-                <div className="text-[.8rem] md:text-lg bottom-0 relative -tracking-tight">
-                    <span className="absolute left-[-9.5rem] md:left-[-13.5rem]">DAY</span>
-                    <span className="absolute left-[-4.5rem] md:left-[-7.5rem]">HOURS</span>
-                    <span className="absolute left-[.8rem] md:left-[.3rem]">MINUTES</span>
-                    <span className="absolute left-[6.5rem] md:left-[9rem]">SECONDS</span>
-                </div>
-            </div>
-            <Image
-                src="/QR.png"
-                alt="barcode"
-                width={150}
-                height={150}
-                className='hidden xl:block object-contain px-4 mt-[8rem] border border-gray/50'
-            />
+        <div className="text-[.8rem] md:text-lg bottom-0 relative -tracking-tight">
+          <span className="absolute left-[-9.5rem] md:left-[-13.5rem]">
+            DAY
+          </span>
+          <span className="absolute left-[-4.5rem] md:left-[-7.5rem]">
+            HOURS
+          </span>
+          <span className="absolute left-[.8rem] md:left-[.3rem]">MINUTES</span>
+          <span className="absolute left-[6.5rem] md:left-[9rem]">SECONDS</span>
+        </div>
+      </div>
+      <Image
+        src="/QR.png"
+        alt="barcode"
+        width={150}
+        height={150}
+        className="hidden xl:block object-contain px-4 mt-[8rem] border border-gray/50"
+      />
 
-            <div className='hidden xl:block pl-4 border  min-w-min-[3rem] border-gray/50 p-3 mt-[8rem] text-white'>
-                <div className='flex flex-col gap-2 justify-center items-center h-[100%]'>
-                    <span className='font-chakra text-[1.8rem]'>2023 <span className='font-clash'>April</span></span>
-                    <span className='flex gap-2 text-[2rem] font-chakra font-bold'>26
-                        <b className='text-[12px]'>TH</b>
-                        27
-                        <b className='text-[12px]'>TH</b>
-                    </span>
-                </div>
-            </div>
+      <div className="hidden xl:block pl-4 border  min-w-min-[3rem] border-gray/50 p-3 mt-[8rem] text-white">
+        <div className="flex flex-col gap-2 justify-center items-center h-[100%]">
+          <span className="font-chakra text-[1.8rem]">
+            2023 <span className="font-clash">April</span>
+          </span>
+          <span className="flex gap-2 text-[2rem] font-chakra font-bold">
+            26
+            <b className="text-[12px]">TH</b>
+            27
+            <b className="text-[12px]">TH</b>
+          </span>
+        </div>
+      </div>
 
-            {/* <div className='hidden xl:block p-3 mt-[8rem] border border-gray/50'>
+      {/* <div className='hidden xl:block p-3 mt-[8rem] border border-gray/50'>
                 <div className='flex flex-col gap-2 justify-center items-center h-[100%]'>
                     <div className='flex gap-2 '>
                         <div className='bg-white/50 box h-2 w-2'></div>
@@ -84,8 +93,8 @@ function Clock() {
             <div className='hidden xl:block p-3 mt-[8rem] grow-[2] border border-gray/50 text-white italic font-clash text-[4rem] font-bold'>
                 <h1 className='fli flex items-center w-full h-full justify-center'>LE<span id="offset">T</span>'S<span id="offset"> &nbsp;&nbsp;R</span>OC<span id="offset">K</span></h1>
             </div> */}
-        </div>
-    )
+    </div>
+  );
 }
 
-export default Clock
+export default Clock;
--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,48 +1,65 @@
-import React from 'react'
+import React from "react";
 
 export default function Faq() {
-    return (
-        <div className='px-2 py-4 xl:px-20'>
-            <div className='flex justify-start w-full font-clash font-bold'>
-                <span className='text-white/10 text-[5.5rem] xl:text-[7rem]'>FAQ</span>
+  return (
+    <div className="px-2 py-4 xl:px-20">
+      <div className="flex justify-start w-full font-clash font-bold">
+        <span className="text-white/10 text-[5.5rem] xl:text-[7rem]">FAQ</span>
+      </div>
+
+      <div className="flex flex-col gap-4 xl:p-2">
+        <div className="font-chakra">
+          <div className="border-t-2 border-b-2 border-gray/30">
+            <input
+              id="collapsible1"
+              className="toggle hidden"
+              type="checkbox"
+            />
+            <label
+              htmlFor="collapsible1"
+              className="lbl-toggle block cursor-pointer font-semibold text-white/80 text-xl md:text-2xl p-7"
+            >
+              What are the available transportation options for commuting to
+              college?
+            </label>
+            <div className="collapsible-content">
+              <div className="content-inner text-white px-4">
+                <p>
+                  Just come and see for yourself. We are a group of people who
+                  are passionate about technology and want to share that passion
+                  with others.
+                </p>
+              </div>
             </div>
-
-            <div className='flex flex-col gap-4 xl:p-2'>
-                <div className="font-chakra">
-                    <div className='border-t-2 border-b-2 border-gray/30'>
-                        <input id="collapsible1" className="toggle hidden" type="checkbox" />
-                        <label htmlFor="collapsible1" className="lbl-toggle block cursor-pointer font-semibold text-white/80 text-xl md:text-2xl p-7">
-                            What are the available transportation options for commuting to college?
-                        </label>
-                        <div className="collapsible-content">
-                            <div className="content-inner text-white px-4">
-                                <p>
-                                    Just come and see for yourself. We are a group of people who are passionate about technology and want to
-                                    share that passion with others.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="font-chakra">
-                    <div className='border-t-2 border-b-2  border-gray/30'>
-                        <input id="collapsible2" className="toggle hidden" type="checkbox" />
-                        <label htmlFor="collapsible2" className="lbl-toggle block cursor-pointer font-semibold text-white/80 text-xl md:text-2xl p-7">
-                            What is the allocation of activity points for individuals who participate in the event?
-                        </label>
-                        <div className="collapsible-content">
-                            <div className="content-inner text-white px-4">
-                                <p>
-                                    Just come and see for yourself. We are a group of people who are passionate about technology and want to
-                                    share that passion with others.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+          </div>
         </div>
-    )
+
+        <div className="font-chakra">
+          <div className="border-t-2 border-b-2  border-gray/30">
+            <input
+              id="collapsible2"
+              className="toggle hidden"
+              type="checkbox"
+            />
+            <label
+              htmlFor="collapsible2"
+              className="lbl-toggle block cursor-pointer font-semibold text-white/80 text-xl md:text-2xl p-7"
+            >
+              What is the allocation of activity points for individuals who
+              participate in the event?
+            </label>
+            <div className="collapsible-content">
+              <div className="content-inner text-white px-4">
+                <p>
+                  Just come and see for yourself. We are a group of people who
+                  are passionate about technology and want to share that passion
+                  with others.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
