848d78a9692f378e140ad65cc0f60b838d18f0f0
gladsondezy <gladson@dezyit.com>
2023-04-19 23:23:53 +0530

    add events

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function NotFound() {
   return (
@@ -10,13 +10,23 @@ export default function NotFound() {
         width={400}
         height={400}
         alt="logo"
-        className='w-[12rem] h-[12rem] xl:w-auto xl:h-auto'
+        className="w-[12rem] h-[12rem] xl:w-auto xl:h-auto"
       />
-      <div className='flex flex-col gap-3'>
-        <h1 className='text-lg md:text-xl font-medium font-ibm'> <b className='text-3xl'>Oops</b> page is not found !</h1>
-        <p className='text-sm font-medium font-ibm'>You just hit a route that doesn't exist...</p>
-        <Link className='text-sm font-medium font-ibm text-main_primary' href="/">Go back</Link>
+      <div className="flex flex-col gap-3">
+        <h1 className="text-lg md:text-xl font-medium font-ibm">
+          {" "}
+          <b className="text-3xl">Oops</b> page is not found !
+        </h1>
+        <p className="text-sm font-medium font-ibm">
+          You just hit a route that doesn't exist...
+        </p>
+        <Link
+          className="text-sm font-medium font-ibm text-main_primary"
+          href="/"
+        >
+          Go back
+        </Link>
       </div>
-    </div >
-  )
+    </div>
+  );
 }
