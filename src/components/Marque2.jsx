eb2e9a1defa519b4eea8b1d93692f695c8852c97
vishalkrishnads <321vishalds@gmail.com>
2024-02-27 21:09:17 +0530

    Fixed build error

--- src/components/Marque2.jsx
+++ src/components/Marque2.jsx
@@ -7,14 +7,14 @@ export default function Marque2() {
         <section className='flex flex-col relative h-[12rem] pt-6 overflow-hidden'>
             <div className='marquee2 font-clash tracking-widest text-main_primary/50'>
                 {Array.from({ length: 3 }).map((_, index) => {
-                    return <h4 className="text font-black">SOLVE-CREATE-THRIVE</h4>
+                    return <h4 key={index} className="text font-black">SOLVE-CREATE-THRIVE</h4>
                 })}
             </div>
 
             <div className='marquee3 font-chakra tracking-widest text-main_primary/50'>
             {Array.from({ length: 5 }).map((_, index) => {
                 return <>
-                    <h1 className="text font-black textStroke text-transparent">{shortText}{String.fromCharCode(160).repeat(longText.length - shortText.length)}</h1>
+                    <h1 key={index} className="text font-black textStroke text-transparent">{shortText}{String.fromCharCode(160).repeat(longText.length - shortText.length)}</h1>
                     <h1 className="text font-black">{longText}</h1>
                 </>
             })}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,8 +13,8 @@ import gsap from "gsap";
 import fsPromises from "fs/promises";
 import path from "path";
 import RitModel from "@/components/RitModel";
-import EventSlider from "@/components/EventSlider";
-import Partical from "@/components/Partical";
+// import EventSlider from "@/components/EventSlider";
+// import Partical from "@/components/Partical";
 
 const Home = () => {
   const [isLoaded, setIsLoaded] = useState(false);
