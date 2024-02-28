9c97980e97f994c60a7ac6a6077f19153375798c
Vishal <321vishalds@gmail.com>
2024-02-28 23:30:15 +0530

    Fixed social media previews (#8)

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -3,25 +3,27 @@ import { useRouter } from "next/router";
 
 export default function Title({ title, description }) {
     const router = useRouter()
+    const domain = "https://yukthi.org"
+
     return <Head>
         <title>{title}</title>
-        <link rel="shortcut icon" href="/favicon.ico" type="image/png" />
+        <link rel="shortcut icon" href={`${domain}/favicon.ico`} type="image/png" />
 
         <meta name="title" content={title} />
         <meta name="description" content={description} />
 
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content={`https://yukthi.org${router.pathname}`} />
+        <meta property="og:url" content={`${domain}${router.pathname}`} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content="/favicon.png" />
+        <meta property="og:image" content={`${domain}/twitter.png`} />
 
         {/* Twitter */}
-        <meta property="twitter:card" content="/twitter.png" />
-        <meta property="twitter:url" content={`https://yukthi.org${router.pathname}`} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={`${domain}${router.pathname}`} />
         <meta property="twitter:title" content={title} />
         <meta property="twitter:description" content={description} />
-        <meta property="twitter:image" content="/favicon.png" />
+        <meta property="twitter:image" content={`${domain}/twitter.png`} />
     </Head>
 }
